19375d7202c9a397638c856c8b25c17addb40c1d
Vlad Pavlyuk <vladpavluk57@gmail.com>
2018-07-02 12:10:59 +0300

    Changed Validate method. Added prefix this whenever it needs. Improve code style.

--- StudentList/Fragments/StudentProfileFragment.cs
+++ StudentList/Fragments/StudentProfileFragment.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -18,23 +19,23 @@ using StudentList;
 using StudentList.Constants;
 using StudentList.Model;
 using StudentList.Providers.Interfaces;
-using System.Threading.Tasks;
 
 namespace StudentList.Fragments
 {
     public class StudentProfileFragment : Android.Support.V4.App.Fragment
     {
-        private string StudentId => Arguments.GetString(IntentConstant.StudentId, string.Empty);
-        private bool NewStudent => Arguments.GetBoolean(IntentConstant.NewStudent, false);
+        private string StudentId => this.Arguments.GetString(IntentConstant.StudentId, string.Empty);
+
+        private bool NewStudent => this.Arguments.GetBoolean(IntentConstant.NewStudent, false);
 
         private IStudentRepository studentRepository;
 
         private Button saveButton;
-        private TextInputLayout nameEditText;
-        private TextInputLayout birthdateEditText;
-        private TextInputLayout universityEditText;
-        private TextInputLayout groupEditText;
-        private TextInputLayout phoneEditText;
+        private TextInputLayout nameLayout;
+        private TextInputLayout birthdateLayout;
+        private TextInputLayout universityLayout;
+        private TextInputLayout groupLayout;
+        private TextInputLayout phoneLayout;
 
         public static StudentProfileFragment NewInstance(string studentId, bool newStudent)
         {
@@ -48,44 +49,45 @@ namespace StudentList.Fragments
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            HasOptionsMenu = true;
+
+            this.studentRepository = new StudentsRepository();
+            this.HasOptionsMenu = true;
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var view = inflater.Inflate(Resource.Layout.student_profile, null);
 
-            saveButton = view.FindViewById<Button>(Resource.Id.save_changes_btn);
-            nameEditText = view.FindViewById<TextInputLayout>(Resource.Id.name_layout);
-            birthdateEditText = view.FindViewById<TextInputLayout>(Resource.Id.birthdate_layout);
-            universityEditText = view.FindViewById<TextInputLayout>(Resource.Id.uni_layout);
-            groupEditText = view.FindViewById<TextInputLayout>(Resource.Id.group_layout);
-            phoneEditText = view.FindViewById<TextInputLayout>(Resource.Id.phone_layout);
+            this.saveButton = view.FindViewById<Button>(Resource.Id.save_changes_btn);
+            this.nameLayout = view.FindViewById<TextInputLayout>(Resource.Id.name_layout);
+            this.birthdateLayout = view.FindViewById<TextInputLayout>(Resource.Id.birthdate_layout);
+            this.universityLayout = view.FindViewById<TextInputLayout>(Resource.Id.uni_layout);
+            this.groupLayout = view.FindViewById<TextInputLayout>(Resource.Id.group_layout);
+            this.phoneLayout = view.FindViewById<TextInputLayout>(Resource.Id.phone_layout);
 
             return view;
         }
 
         public override async void OnViewCreated(View view, Bundle savedInstanceState)
         {
-            studentRepository = new StudentsRepository();
-            var selectedStudent = await this.studentRepository.GetStudentById(StudentId);
+            var selectedStudent = await this.studentRepository.GetStudentById(this.StudentId);
 
-            ((AppCompatActivity)Activity).SupportActionBar.Title = NewStudent ? GetString(Resource.String.create_student_title) : 
-                GetString(Resource.String.edit_student_title) + "" + selectedStudent.Name;
+            ((AppCompatActivity)this.Activity).SupportActionBar.Title = this.NewStudent ? this.GetString(Resource.String.create_student_title)
+                : this.GetString(Resource.String.edit_student_title) + " " + selectedStudent.Name;
 
-            saveButton.Text = NewStudent ? GetString(Resource.String.add_new_student_text) : GetString(Resource.String.save_changes_text);
-            nameEditText.EditText.Text = NewStudent ? "" : selectedStudent.Name;
-            birthdateEditText.EditText.Text = NewStudent ? "" : selectedStudent.Birthdate.ToShortDateString();
-            universityEditText.EditText.Text = NewStudent ? "" : selectedStudent.University;
-            groupEditText.EditText.Text = NewStudent ? "" : selectedStudent.GroupName;
-            phoneEditText.EditText.Text = NewStudent ? "" : selectedStudent.Phone;
+            this.saveButton.Text = this.NewStudent ? this.GetString(Resource.String.add_new_student_text)
+                : this.GetString(Resource.String.save_changes_text);
+            this.nameLayout.EditText.Text = this.NewStudent ? string.Empty : selectedStudent.Name;
+            this.birthdateLayout.EditText.Text = this.NewStudent ? string.Empty : selectedStudent.Birthdate.ToShortDateString();
+            this.universityLayout.EditText.Text = this.NewStudent ? string.Empty : selectedStudent.University;
+            this.groupLayout.EditText.Text = this.NewStudent ? string.Empty : selectedStudent.GroupName;
+            this.phoneLayout.EditText.Text = this.NewStudent ? string.Empty : selectedStudent.Phone;
         }
 
         public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
         {
             menu.Clear();
             inflater.Inflate(Resource.Menu.confirm_menu, menu);
-            base.OnCreateOptionsMenu(menu, inflater);
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
@@ -93,13 +95,13 @@ namespace StudentList.Fragments
             switch (item.ItemId)
             {
                 case Resource.Id.action_confirm:
-                    Confirm();
+                    this.Confirm();
                     return true;
                 case Resource.Id.action_reset:
-                    Reset();
+                    this.Reset();
                     return true;
                 case Android.Resource.Id.Home:
-                    Activity.OnBackPressed();
+                    this.Activity.OnBackPressed();
                     return true;
             }
 
@@ -109,24 +111,28 @@ namespace StudentList.Fragments
         public override void OnStart()
         {
             base.OnStart();
-            saveButton.Click += SaveButton_Click;
-            birthdateEditText.EditText.Touch += OnBirthdateEditTextTouch;
-            birthdateEditText.EditText.FocusChange += OnBirthdateEditTextFocus;
-            DisplayHomeUp(true);
+
+            this.saveButton.Click += this.SaveButton_Click;
+            this.birthdateLayout.EditText.Touch += this.OnBirthdateEditTextTouch;
+            this.birthdateLayout.EditText.FocusChange += this.OnBirthdateEditTextFocus;
+            this.DisplayHomeUp(true);
         }
 
         public override void OnStop()
         {
             base.OnStop();
-            saveButton.Click -= SaveButton_Click;
-            birthdateEditText.Touch -= OnBirthdateEditTextTouch;
-            birthdateEditText.EditText.FocusChange -= OnBirthdateEditTextFocus;
-            DisplayHomeUp(false);
+
+            this.saveButton.Click -= this.SaveButton_Click;
+            this.birthdateLayout.Touch -= this.OnBirthdateEditTextTouch;
+            this.birthdateLayout.EditText.FocusChange -= this.OnBirthdateEditTextFocus;
+            this.DisplayHomeUp(false);
         }
 
         private void OnBirthdateEditTextTouch(object sender, View.TouchEventArgs e)
         {
-            var datePicker = new DatePickerDialog(Context, DataSetPickerDialog, DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.Now.Day);
+            var datePicker = new DatePickerDialog(
+                this.Context, this.DataSetPickerDialog, DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.Now.Day);
+
             if (e.Event.Action == MotionEventActions.Down)
             {
                 datePicker.Show();
@@ -137,40 +143,54 @@ namespace StudentList.Fragments
         {
             if (e.HasFocus)
             {
-                var datePicker = new DatePickerDialog(Context, DataSetPickerDialog, DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.Now.Day);
+                var datePicker = new DatePickerDialog(
+                    this.Context, this.DataSetPickerDialog, DateTime.Now.Year, DateTime.Now.Month - 1, DateTime.Now.Day);
                 datePicker.Show();
             }
         }
 
         private void DataSetPickerDialog(object sender, DatePickerDialog.DateSetEventArgs e)
         {
-            birthdateEditText.EditText.Text = e.Date.ToShortDateString();
+            this.birthdateLayout.EditText.Text = e.Date.ToShortDateString();
         }
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            Confirm();
+            this.Confirm();
         }
 
         private void Confirm()
         {
-            if (Validate(nameEditText, birthdateEditText, groupEditText, universityEditText))
-                return;
-            string id = Guid.NewGuid().ToString();
-            string name = nameEditText.EditText.Text.TrimEnd();
-            DateTime birthdate = Convert.ToDateTime(birthdateEditText.EditText.Text);
-            string uni = universityEditText.EditText.Text.TrimEnd();
-            string group = groupEditText.EditText.Text.TrimEnd();
-            string phone = phoneEditText.EditText.Text.TrimEnd().Length == 0 ? null : phoneEditText.EditText.Text.TrimEnd();
+            string name = this.nameLayout.EditText.Text.TrimEnd();
+            string birthdate = this.birthdateLayout.EditText.Text;
+            string uni = this.universityLayout.EditText.Text.TrimEnd();
+            string group = this.groupLayout.EditText.Text.TrimEnd();
+            string phone = this.phoneLayout.EditText.Text.TrimEnd().Length == 0 ? null
+                : this.phoneLayout.EditText.Text.TrimEnd();
 
-            if (NewStudent)
+            if (this.Validate(name, birthdate, group, uni))
             {
-                var student = new Student() { Id = id, Name = name, Birthdate = birthdate, University = uni, GroupName = group, Phone = phone };
-                studentRepository.AddNewStudent(student);
-            }
-            else
-            {
-                studentRepository.ChangeStudentById(StudentId, name, birthdate, group, uni, phone);
+                string id = Guid.NewGuid().ToString();
+
+                if (this.NewStudent)
+                {
+                    var student = new Student()
+                    {
+                        Id = id,
+                        Name = name,
+                        Birthdate = Convert.ToDateTime(birthdate, CultureInfo.InvariantCulture),
+                        University = uni,
+                        GroupName = group,
+                        Phone = phone
+                    };
+
+                    this.studentRepository.AddNewStudent(student);
+                }
+                else
+                {
+                    this.studentRepository.ChangeStudentById(
+                        this.StudentId, name, Convert.ToDateTime(birthdate, CultureInfo.InvariantCulture), group, uni, phone);
+                }
             }
 
             this.Activity.OnBackPressed();
@@ -178,36 +198,63 @@ namespace StudentList.Fragments
 
         private void DisplayHomeUp(bool trigger)
         {
-            ((AppCompatActivity)Activity).SupportActionBar.SetDisplayHomeAsUpEnabled(trigger && Activity.SupportFragmentManager.BackStackEntryCount > 0);
+            ((AppCompatActivity)this.Activity)
+                .SupportActionBar
+                .SetDisplayHomeAsUpEnabled(trigger && this.Activity.SupportFragmentManager.BackStackEntryCount > 0);
         }
 
         private void Reset()
         {
-            nameEditText.EditText.Text = string.Empty;
-            groupEditText.EditText.Text = string.Empty;
-            birthdateEditText.EditText.Text = string.Empty;
-            universityEditText.EditText.Text = string.Empty;
-            phoneEditText.EditText.Text = string.Empty;
+            this.nameLayout.EditText.Text = string.Empty;
+            this.groupLayout.EditText.Text = string.Empty;
+            this.birthdateLayout.EditText.Text = string.Empty;
+            this.universityLayout.EditText.Text = string.Empty;
+            this.phoneLayout.EditText.Text = string.Empty;
         }
 
-        private bool Validate(params TextInputLayout[] inputLayout)
+        private bool Validate(string name, string birthdate, string group, string uni)
         {
             bool validation = false;
 
-            for (int i = 0; i < inputLayout.Length; i++)
-            {
-                if (string.IsNullOrWhiteSpace(inputLayout[i].EditText.Text) || string.IsNullOrEmpty(inputLayout[i].EditText.Text))
-                {
-                    inputLayout[i].Error = " ";
-                    validation = true;
-                }
-                else
-                {
-                    inputLayout[i].Error = string.Empty;
-                }
-            }
+            this.IsEmptyOrWhiteSpace(name, this.nameLayout, ref validation);
+            this.IsEmptyOrWhiteSpace(birthdate, this.birthdateLayout, ref validation);
+            this.IsEmptyOrWhiteSpace(group, this.groupLayout, ref validation);
+            this.IsEmptyOrWhiteSpace(uni, this.universityLayout,  ref validation);
 
             return validation;
         }
+
+        private void IsEmptyOrWhiteSpace(string name, TextInputLayout targetLayout, ref bool validation)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+            {
+                targetLayout.Error = " ";
+                validation = true;
+            }
+            else
+            {
+                targetLayout.Error = string.Empty;
+            }
+        }
+
+        //private bool Validate(params TextInputLayout[] inputLayout)
+        //{
+        //    bool validation = false;
+
+        //    for (int i = 0; i < inputLayout.Length; i++)
+        //    {
+        //        if (string.IsNullOrWhiteSpace(inputLayout[i].EditText.Text) || string.IsNullOrEmpty(inputLayout[i].EditText.Text))
+        //        {
+        //            inputLayout[i].Error = " ";
+        //            validation = true;
+        //        }
+        //        else
+        //        {
+        //            inputLayout[i].Error = string.Empty;
+        //        }
+        //    }
+
+        //    return validation;
+        //}
     }
 }

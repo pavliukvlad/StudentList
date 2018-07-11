bb3cf3d6f50b0a3f553e01e4ed4bad3c49439404
Vlad Pavlyuk <vladpavluk57@gmail.com>
2018-07-11 16:48:51 +0300

    Added a default property.

--- StudentList/Models/StudentFilter.cs
+++ StudentList/Models/StudentFilter.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 
-namespace StudentList.Model
+namespace StudentList.Models
 {
     public class StudentFilter
     {
+        public static StudentFilter Default => new StudentFilter()
+        {
+            Name = null,
+            Group = null,
+            Birthdate = DateTime.MinValue
+        };
+
         public string Name { get; set; }
 
         public string Group { get; set; }
 
         public DateTime Birthdate { get; set; }
-
-        public static StudentFilter Default => default(StudentFilter);
     }
 }
